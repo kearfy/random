862f1f27509da3e3141bcc9961db4399c11effc9
Kearfy <mt.dev@hotmail.com>
2019-06-21 08:53:56 +0200

    Update wsmng.js

--- wsmng.js
+++ wsmng.js
@@ -1,39 +1,39 @@
 class wsmng {
-	static send(msg) {
-		window.InputEvent = window.Event || window.InputEvent;
+		static send(msg) {
+			window.InputEvent = window.Event || window.InputEvent;
 
-  		var event = new InputEvent('input', {
-   			bubbles: true
-  		});
+	  		var event = new InputEvent('input', {
+	   			bubbles: true
+	  		});
 
-  		var textbox = document.querySelector('div._2S1VP');
+	  		var textbox = document.querySelector('div._3u328');
 
-  		textbox.textContent = msg;
-  		textbox.dispatchEvent(event);
+	  		textbox.textContent = msg;
+	  		textbox.dispatchEvent(event);
 
-  		document.querySelector("button._35EW6").click();
-	}
+	  		document.querySelector("button._3M-N-").click();
+		}
 
-	static start(msg, interval, amount = false) {
-		interval = (interval < 1 ? 1 : interval);
-		var counter = 0;
-		if (!amount) {
-			return setInterval(() => {
-				this.send(msg);	
-			}, interval);
-		} else {
-			var bomber = setInterval(() => {
-				this.send(msg);
-				counter += 1;
-				if (counter === amount) {
-					clearInterval(bomber);
-					console.log('finished after ' + amount + ' messages');
-				}
-			}, interval);
+		static start(msg, interval, amount = false) {
+			interval = (interval < 1 ? 1 : interval);
+			var counter = 0;
+			if (!amount) {
+				return setInterval(() => {
+					this.send(msg);
+				}, interval);
+			} else {
+				var bomber = setInterval(() => {
+					this.send(msg);
+					counter += 1;
+					if (counter === amount) {
+						clearInterval(bomber);
+						console.log('finished after ' + amount + ' messages');
+					}
+				}, interval);
+			}
+		}
+
+		static stop(interval) {
+			clearInterval(interval);
 		}
 	}
-
-	static stop(interval) {
-		clearInterval(interval);
-	}
-}
